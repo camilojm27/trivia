45b6f84be32ef0186eecac9a4206b8ce45e1dd90
Camilo José Mezú Mina <camilo272001@gmail.com>
2019-11-23 13:51:14 -0500

    Comit inicial
    Ya está la pantalla de inicio

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+void main() => runApp(Trivia());
+
+class Trivia extends StatelessWidget {
+  static const _titulo = 'Trivia';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _titulo,
+      home: AndroidTriviaHome(title: _titulo,),
+    );
+  }
+}
+
+class AndroidTriviaHome extends StatelessWidget {
+  AndroidTriviaHome({Key key, this.title}) : super(key:key);
+
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title),),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              
+              SvgPicture.asset('assets/images/android_trivia.svg'),
+              RaisedButton(
+                onPressed: () {},
+                child: Text('PLAY', style: TextStyle(color: Colors.purple),),
+                color: Colors.grey.shade400,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
